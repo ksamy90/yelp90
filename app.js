96b418c5ee818df05753360bc87e6fcc82fbfa71
ksamy90 <ksamy90@aol.com>
2017-02-10 07:03:19 +0000

    Modify app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v9");
+mongoose.connect("mongodb://moshyl:1040jazz@ds015700.mlab.com:15700/yelp90");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
